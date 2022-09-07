a1443272f9b2bcc62d84e2bdabaf2ca2faaa3a4b
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-09-07 22:37:26 +0000

    Betty styling

--- cmd_args.c
+++ cmd_args.c
@@ -8,33 +8,35 @@
  */
 char **cmd_args(char *arg, const char *delim)
 {
-    char **args;
-    size_t len = 5, i = 0;
-    if (arg == NULL)
-    {
-        perror("./hsh");
-        return (NULL);
-    }
-    args = malloc(len * sizeof(char *));
-    if (args == NULL)
-    {
-        perror("./hsh");
-        return (NULL);
-    }
-    args[0] = strtok(arg, delim);
-    while (args[i] != NULL)
-    {
-        i++;
-        if (i == len)
-        {
-            args = __realloc(args, len);
-            if (args == NULL)
-            {
-                perror("./hsh");
-                return (NULL);
-            }
-        }
-        args[i] = strtok(NULL, delim);
-    }
-    return (args);
+char **args;
+size_t len = 5, i = 0;
+if (arg == NULL)
+{
+perror("./hsh");
+return (NULL);
 }
+args = malloc(len * sizeof(char *));
+if (args == NULL)
+{
+perror("./hsh");
+return (NULL);
+}
+args[0] = strtok(arg, delim);
+while (args[i] != NULL)
+{
+i++;
+if (i == len)
+{
+args = __realloc(args, len);
+if (args == NULL)
+{
+perror("./hsh");
+return (NULL);
+}
+}
+args[i] = strtok(NULL, delim);
+}
+return (args);
+}
+
+
