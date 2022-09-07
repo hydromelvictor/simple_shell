d0646864c97ea8304234c2b6bac572edb839b536
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-09-07 22:28:33 +0000

    Betty styling

--- realloc.c
+++ realloc.c
@@ -8,20 +8,21 @@
  */
 char **__realloc(char **args, size_t n)
 {
-    char **sstr;
-    size_t i = 0;
-    sstr = malloc((n + 5) * sizeof(char *));
-    if (sstr == NULL)
-    {
-        perror("Fatal Error");
-        free(args);
-        return (NULL);
-    }
-    for (i = 0; i < n; i++)
-    {
-        sstr[i] = args[i];
-    }
-    n += 5;
-    free(args);
-    return (sstr);
+char **sstr;
+size_t i = 0;
+sstr = malloc((n + 5) * sizeof(char *));
+if (sstr == NULL)
+{
+perror("Fatal Error");
+free(args);
+return (NULL);
 }
+for (i = 0; i < n; i++)
+{
+sstr[i] = args[i];
+}
+n += 5;
+free(args);
+return (sstr);
+}
+
