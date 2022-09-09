bcc6a904721c7256cb121d4e5b5dc411fc35d305
harkinder-dark <victorvaddely@gmail.com>
2022-09-09 08:49:32 +0000

    shell simple

--- built_ins.c
+++ built_ins.c
@@ -9,10 +9,17 @@
 int _chdir(term_t term)
 {
 struct  stat buf;
+char *str;
 if (strcmp(term.argv[0], "cd") == 0)
 {
     if (term.argv[1] != NULL)
     {
+        if (term.argv[1][0] == '$')
+        {
+            str = malloc(strlen(term.argv[1]) * sizeof(char) - 1);
+            str = term.argv[1] + 1;
+            term.argv[1] = _getenv(str);
+        }
         if (stat(term.argv[1], &buf) == 0)
         {
             return (chdir(term.argv[1]));
