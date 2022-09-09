e1575338ded549642b151e815e1c5f3c0fa73ef4
harkinder-dark <victorvaddely@gmail.com>
2022-09-09 13:58:51 +0000

    shell simple

--- shell.c
+++ shell.c
@@ -1,43 +1,48 @@
 #include "shell.h"
+/**
+ * main - the main
+ *
+ * Return: int
+ */
 int main(void)
 {
-    term_t term = {NULL, NULL, NULL, NULL, NULL, 0};
-    size_t n = 0, hub = 0, i;
+term_t term = {NULL, NULL, NULL, NULL, NULL, 0};
+size_t n = 0, hub = 0, i;
 
-    if (isatty(STDIN_FILENO) == 0)
-    {
-        hub = 1;
-    }
-    if (hub == 0)
-    {
-        printf("$ ");
-    }
+if (isatty(STDIN_FILENO) == 0)
+{
+hub = 1;
+}
+if (hub == 0)
+{
+printf("$ ");
+}
     
-    while (getline(&term.line, &n, stdin) != EOF)
-    {
-        term.cmd_lines = cmd_args(term.line, ";");
-        for (i = 0; term.cmd_lines[i] != NULL; i++)
-        {
-            term.argv = cmd_args(term.cmd_lines[i], " \t\r\n\a");
-            if (cmd_sys(term) == 400)
-            {
-                free(term.line);
-                free(term.envp);
-                free(term.cmd_lines);
-                free(term.argv);
-                free(term.cmd);
-                exit(term.status);
-            }
-            free(term.argv);
-            free(term.cmd);
-        }
-        if (hub == 0)
-        {
-            printf("$ ");
-        }
-        free(term.cmd_lines);
-    }
-    free(term.line);
-    free(term.envp);
-    exit(term.status);
+while (getline(&term.line, &n, stdin) != EOF)
+{
+term.cmd_lines = cmd_args(term.line, ";");
+for (i = 0; term.cmd_lines[i] != NULL; i++)
+{
+term.argv = cmd_args(term.cmd_lines[i], " \t\r\n\a");
+if (cmd_sys(term) == 400)
+{
+free(term.line);
+free(term.envp);
+free(term.cmd_lines);
+free(term.argv);
+free(term.cmd);
+exit(term.status);
+}
+free(term.argv);
+free(term.cmd);
+}
+if (hub == 0)
+{
+printf("$ ");
+}
+free(term.cmd_lines);
+}
+free(term.line);
+free(term.envp);
+exit(term.status);
 }
